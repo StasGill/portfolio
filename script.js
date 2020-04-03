f4f1e23e3e61acbdf7fb2411138ffddcfdaf4f3a
Your Name <stasgill@gmail.com>
2020-04-03 21:42:08 +0300

    menuCorection

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ let change = true
 burgerMenu.addEventListener('click',(event)=>{  
     
      if(change){                           
-     navigation.style=`position: absolute;left: 0px`;
+     navigation.style=`position: fixed;left: 0px;transition: transform 0.3s ease-in-out;`;
      event.preventDefault();
      change = false
      console.log(change)
-     }else{ navigation.style=`background-color: none; `;
+     }else{ navigation.style=`position: fixed;left: -320px;transition: transform 0.3s ease-in-out;`;
       change = true;
       event.preventDefault();
       console.log(change)
