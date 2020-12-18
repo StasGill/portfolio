75aa2bf626acc407766a7692a7d530974d15c285
Your Name <stasgill@gmail.com>
2020-12-18 12:31:49 +0200

    Repair burger

--- script.js
+++ script.js
@@ -5,16 +5,23 @@ console.log(navigation)
 
 let change = true   
 
-burgerMenu.addEventListener('click',(event)=>{  
+// burgerMenu.addEventListener('click',(event)=>{  
     
-     if(change){                           
-     navigation.style=`position: fixed;left: 0px;transition: transform 0.3s ease-in-out;`;
-     event.preventDefault();
-     change = false
-     console.log(change)
-     }else{ navigation.style=`position: fixed;left: -320px;transition: transform 0.3s ease-in-out;`;
-      change = true;
-      event.preventDefault();
-      console.log(change)
-    }
+//      if(change){                           
+//      navigation.style=`position: fixed;left: 0px;transition: transform 0.3s ease-in-out;`;
+//      event.preventDefault();
+//      change = false
+//      console.log(change)
+//      }else{ navigation.style=`position: fixed;left: -320px;transition: transform 0.3s ease-in-out;`;
+//       change = true;
+//       event.preventDefault();
+//       console.log(change)
+//     }
+//     })
+
+
+    //elem.classList.toggle(cls)
+
+    burgerMenu.addEventListener('click',(event)=>{
+      navigation.classList.toggle(navigationsActive)
     })
